c98c8a727464b0560219f2bb10732085af533038
limmlingg <limmlingg@gmail.com>
2018-01-28 03:10:04 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# weather
+
+A site that visually shows you the weather in your area. Made for Hack&Roll 2018.
+
+#### Acknowledgements
+
+* openweather for weather information
+* Google Maps for area information
+* The internet for teaching me how to do jQuery
---
+++ script.js
@@ -0,0 +1,64 @@
+var width = window.innerWidth;
+var height = window.innerHeight;
+var lat, lon; // Latitude and longitude of location
+var weatherType; // Used to generate animation
+
+// Get location
+function getLocation(callbackWeather, callbackMap) {
+	navigator.geolocation.getCurrentPosition(function(p) {
+		lat = p.coords.latitude;
+		lon = p.coords.longitude;
+		console.log(lat + "," + lon);
+		callbackWeather();
+		callbackMap();
+	}, function() {
+		console.log("Unable to get location");
+	});
+}
+
+// Call openweather api
+function getWeather() {
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=f0226db01036e625782a08842bf20938", function(data) {
+		// console.log(data);
+		weatherType = data.weather[0].main;
+		$("#forecastText").html(data.weather[0].description); // Update weather
+	});
+}
+
+// Call Google Maps api
+function getMap() {
+	// Check if there are any Streetview images at that location
+	$.getJSON("https://maps.googleapis.com/maps/api/streetview/metadata?size=" + height + "x" + width + "&location=" + lat + "," + lon + "&key=AIzaSyAAr7n_meQxdm_sC6wta1hFIm8gaYEkz_Y", function(d) {
+		//console.log(d.status);
+		if (d.status=="OK") { // Update background image if Streetview image is found
+			var imgURL = "https://maps.googleapis.com/maps/api/streetview?size=" + height + "x" + width + "&location=" + lat + "," + lon + "&key=AIzaSyAAr7n_meQxdm_sC6wta1hFIm8gaYEkz_Y";
+		} else {
+			var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=18&size=" + width + "x" + height+ "&maptype=satellite&key=AIzaSyDL5jkHCL3Jhe409CEeG83oW2DU3XqqTu4";
+		}
+		console.log(imgURL);
+		$("body").css('background-image', 'url(' + imgURL + ')');
+	})
+}
+
+// Overlay the screen with rain
+function rain() {
+	console.log("It is raining!");
+}
+
+// Overlay the screen with snow
+function snow() {
+	console.log("It is snowing!");
+}
+
+// Overlay the screen with clouds
+function cloud() {
+	console.log("It is cloudy!");
+}
+
+getLocation(getWeather, getMap); // Get location before checking weather and map
+
+// Make responsive
+window.onresize = function(event) {
+	width = window.innerWidth;
+	height = window.innerHeight;
+}
