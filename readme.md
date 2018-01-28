ad8fae608cd60886a223e2b1923a1d3c23f81298
limmlingg <limmlingg@gmail.com>
2018-01-28 08:58:02 +0800

    Add search function

--- readme.md
+++ readme.md
@@ -2,8 +2,15 @@
 
 A site that visually shows you the weather in your area. Made for Hack&Roll 2018.
 
+## How to use
+
+1. Visit https://limmlingg.github.io/weather/
+2. Allow your browser to detect your location or type in a location into the search bar and press `Find Location`
+3. Wait
+
 #### Acknowledgements
 
 * openweather for weather information
 * Google Maps for area information
 * The internet for teaching me how to do jQuery
+* The person who stole my hackathon freebie while I was having lunch :(
--- script.js
+++ script.js
@@ -3,12 +3,26 @@ var height = window.innerHeight;
 var lat, lon; // Latitude and longitude of location
 var weatherType; // Used to generate animation
 
+// Find location based on given address
+function findLocation() {
+	var search = $("#location").val();
+	search = search.replace(/\s+/g, '+');
+	//console.log("Search for " + search);
+	$.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + search + "&key=AIzaSyAAr7n_meQxdm_sC6wta1hFIm8gaYEkz_Y", function(data) {
+		//console.log(data);
+		lat = data.results[0].geometry.location.lat;
+		lon = data.results[0].geometry.location.lng;
+		getWeather();
+		getMap();
+	});
+}
+
 // Get location
 function getLocation(callbackWeather, callbackMap) {
 	navigator.geolocation.getCurrentPosition(function(p) {
 		lat = p.coords.latitude;
 		lon = p.coords.longitude;
-		console.log(lat + "," + lon);
+		//console.log(lat + "," + lon);
 		callbackWeather();
 		callbackMap();
 	}, function() {
@@ -18,8 +32,8 @@ function getLocation(callbackWeather, callbackMap) {
 
 // Call openweather api
 function getWeather() {
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=f0226db01036e625782a08842bf20938", function(data) {
-		// console.log(data);
+	$.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=f0226db01036e625782a08842bf20938", function(data) {
+		//console.log(data);
 		weatherType = data.weather[0].main;
 		$("#forecastText").html(data.weather[0].description); // Update weather
 	});
@@ -28,14 +42,14 @@ function getWeather() {
 // Call Google Maps api
 function getMap() {
 	// Check if there are any Streetview images at that location
-	$.getJSON("https://maps.googleapis.com/maps/api/streetview/metadata?size=" + height + "x" + width + "&location=" + lat + "," + lon + "&key=AIzaSyAAr7n_meQxdm_sC6wta1hFIm8gaYEkz_Y", function(d) {
+	$.getJSON("https://maps.googleapis.com/maps/api/streetview/metadata?size=" + height + "x" + width + "&location=" + lat + "," + lon + "&key=AIzaSyAAr7n_meQxdm_sC6wta1hFIm8gaYEkz_Y", function(data) {
 		//console.log(d.status);
-		if (d.status=="OK") { // Update background image if Streetview image is found
+		if (data.status=="OK") { // Update background image if Streetview image is found
 			var imgURL = "https://maps.googleapis.com/maps/api/streetview?size=" + height + "x" + width + "&location=" + lat + "," + lon + "&key=AIzaSyAAr7n_meQxdm_sC6wta1hFIm8gaYEkz_Y";
 		} else {
 			var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=18&size=" + width + "x" + height+ "&maptype=satellite&key=AIzaSyDL5jkHCL3Jhe409CEeG83oW2DU3XqqTu4";
 		}
-		console.log(imgURL);
+		//console.log(imgURL);
 		$("body").css('background-image', 'url(' + imgURL + ')');
 	})
 }
@@ -53,6 +67,11 @@ function snow() {
 // Overlay the screen with clouds
 function cloud() {
 	console.log("It is cloudy!");
+}
+
+// Overlay the screen with fog
+function fog() {
+	console.log("It is foggy!");
 }
 
 getLocation(getWeather, getMap); // Get location before checking weather and map
